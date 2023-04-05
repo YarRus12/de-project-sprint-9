f705a2fec838c2caaf79cc96bcc3631b313afe43
YarRus12 <yaroslav.russu@mail.ru>
2023-04-05 12:57:12 +1000

    Добавлено описание в README.md

--- solution/service_stg/src/stg_loader/stg_message_processor_job.py
+++ solution/service_stg/src/stg_loader/stg_message_processor_job.py
@@ -65,44 +65,5 @@ class StgMessageProcessor:
             self._logger.info('READY TO SEND MESSAGE IN KAFKA')
             # передаем обогащенные данные в kakfa
             self._producer.produce(order_data)
-
-            """
-                Выходные данные
-                    {
-            "object_id": 322519,
-            "object_type": "order",
-            "payload": {
-                "id": 322519,
-                "date": "2022-11-19 16:06:36",
-                "cost": 300,
-                "payment": 300,
-                "status": "CLOSED",
-                "restaurant": {
-                    "id": "626a81cfefa404208fe9abae",
-                    "name": "Кофейня №1"
-                },
-                "user": {
-                    "id": "626a81ce9a8cd1920641e296",
-                    "name": "Котова Ольга Вениаминовна"
-                },
-                "products": [
-                    {
-                        "id": "6276e8cd0cf48b4cded00878",
-                        "price": 180,
-                        "quantity": 1,
-                        "name": "РОЛЛ С ТОФУ И ВЯЛЕНЫМИ ТОМАТАМИ",
-                        "category": "Выпечка"
-                    },
-                    {
-                        "id": "6276e8cd0cf48b4cded0086c",
-                        "price": 60,
-                        "quantity": 2,
-                        "name": "ГРИЛАТА ОВОЩНАЯ ПО-МЕКСИКАНСКИ",
-                        "category": "Закуски"
-                    }
-                ]
-            }
-        }
-            """
         # Пишем в лог, что джоб успешно завершен.
         self._logger.info(f"{datetime.utcnow()}: FINISH")
